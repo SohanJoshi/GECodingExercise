586d6ddd4c615e13911fdf2f8abba86a627609d1
SohanJoshi <sohanjoshi1991@gmail.com>
2020-02-27 22:59:13 +0530

    Exercise 2 Base Case Done

--- src/main/com/ge/exercise2/ArrayQuadrantUtil.java
+++ src/main/com/ge/exercise2/ArrayQuadrantUtil.java
@@ -6,13 +6,119 @@ import org.apache.logging.log4j.Logger;
 public class ArrayQuadrantUtil {
     private static final Logger logger = LogManager.getLogger(ArrayQuadrantUtil.class);
 
-    char[][] data;
+    private char[][] data;
+    private int row;
+    private int column;
 
+
+    /**
+     * Constructs the Util class to operate on the given data 
+     * @param data to be operated on
+     */
     public ArrayQuadrantUtil(char[][] data) {
         this.data = data;
+        if(data == null)
+        	return;
+        this.row = data.length - 1;
+        this.column = data[0].length - 1;
+    }
+    
+    /**
+     * Return all of the values in a specific row in order of appearance (left to right)
+     * @param r row from which data will be given
+     * @return the value from the given row in an array
+     */
+    public char[] getRowValues(int r) {
+    	if(data == null || r < 0 || r > this.row)
+    		return null;
+    	
+    	char temp[] = new char[this.column + 1];
+    	for(int i = 0; i <= this.column ; i ++)
+    		temp[i] = data[r][i];
+    	
+    	return temp;
     }
 
-    public char[] getQuadrantValues(int row, int column) {
-        return null;
+    /**
+     * Return all of the values in a specific column in order of appearance (top to bottom)
+     * @param c column from which data will be given
+     * @return the value from the given column in an array
+     */
+    public char[] getColumnValues(int c) {
+    	if(data == null || c < 0 || c > this.column)
+    		return null;
+    	
+    	char temp[] = new char[this.row + 1];
+    	for(int i = 0; i <= this.row ; i ++)
+    		temp[i] = data[i][c];
+    	
+    	return temp;
     }
+    
+    /**
+     * Return all of the values in a specific quadrant in order of appearance (left to right, top to bottom)
+     * 
+     * @param r row of the index
+     * @param c column of the index
+     * @return the value from the quadrant of the given index in an array
+     */
+    public char[] getQuadrantValues(int r, int c) {
+    	if(data == null)
+    		return null;
+    	
+    	int quadrant = findQuadrant(r, c);
+    	
+    	if(quadrant == -1)
+    		return null;
+    	int quadrantRow = ((row + 1) / 2);
+    	int quadrantColumn = ((column + 1) / 2);
+    	char temp[] = new char[quadrantRow * quadrantColumn];
+    	
+    	int quadrantRowOffset = 0;
+    	int quadrantColumnOffset = 0;
+    	
+    	switch(quadrant)
+    	{
+    		case 1 : quadrantColumnOffset = (this.column / 2) + 1;
+    			break;
+    		case 3 : quadrantColumnOffset = (this.column / 2) + 1;
+    		case 2 : quadrantRowOffset = (this.row / 2) + 1;
+    	}
+    	
+    	for(int i = 0; i < quadrantRow; i ++)
+    		for(int j = 0; j < quadrantColumn; j++)
+    			temp[i * quadrantRow + j] = data[quadrantRowOffset + i][quadrantColumnOffset + j];
+    	
+        return temp;
+    }
+
+    /**
+     * 
+     * The value for quadrant in which the given
+     * index with r & c lies will be returned.
+     * Return value will be according to the given strategy. 
+     * 
+     *  0 | 1 
+     * ---|---
+     *  2 | 3 
+     *  
+     * @param r
+     * @param c
+     * @return
+     */
+	private int findQuadrant(int r, int c) {
+		if(r >= 0 && r <= this.row / 2) {
+			if(c >= 0 && c <= this.column / 2)
+				return 0;
+			else if (c > this.column / 2 && c <= this.column)
+				return 2;
+		} else if (r > this.row / 2 && r <= this.row ){
+			if(c >= 0 && c <= this.column / 2)
+				return 1;
+			else if (c > this.column / 2 && c <= this.column)
+				return 3;
+		}
+		
+		return -1;
+	}
 }
--- src/test/com/ge/exercise2/ArrayQuadrantUtilTest.java
+++ src/test/com/ge/exercise2/ArrayQuadrantUtilTest.java
@@ -1,25 +1,68 @@
 package com.ge.exercise2;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-
 public class ArrayQuadrantUtilTest {
 
+	char[][] data = {
+            {'a', 'b', 'c', 'd'},
+            {'e', 'f', 'g', 'h'},
+            {'i', 'j', 'k', 'l'},
+            {'m', 'n', 'o', 'p'}
+    };
+	
+	@Test
+	public void testNullData() {
+		ArrayQuadrantUtil util = new ArrayQuadrantUtil(null);
+        Assert.assertNull(util.getRowValues(0));
+        Assert.assertNull(util.getColumnValues(0));
+        Assert.assertNull(util.getQuadrantValues(0, 0));
+	}
+	
+    @Test
+    public void getRowValuesTest() {
+
+        ArrayQuadrantUtil util = new ArrayQuadrantUtil(data);
+        Assume.assumeNotNull(util.getRowValues(0));
+
+        Assert.assertNull(util.getRowValues(-1));
+        Assert.assertNull(util.getRowValues(4));
+        
+        char[] expectedResult = {'e', 'f', 'g', 'h'};
+        assertArrayEquals(expectedResult, util.getRowValues(1));
+    }
+
+    @Test
+    public void getColumnValuesTest() {
+
+        ArrayQuadrantUtil util = new ArrayQuadrantUtil(data);
+        Assume.assumeNotNull(util.getColumnValues(0));
+
+        Assert.assertNull(util.getColumnValues(-4));
+        Assert.assertNull(util.getColumnValues(5));
+        
+        char[] expectedResult = {'c', 'g', 'k', 'o'};
+        assertArrayEquals(expectedResult, util.getColumnValues(2));
+    }
+
+    
     @Test
     public void getQuadrantValuesTest() {
-        char[][] data = {
-                {'a', 'b', 'c', 'd'},
-                {'e', 'f', 'g', 'h'},
-                {'i', 'j', 'k', 'l'},
-                {'m', 'n', 'o', 'p'}
-        };
 
         ArrayQuadrantUtil util = new ArrayQuadrantUtil(data);
         Assume.assumeNotNull(util.getQuadrantValues(0, 0));
 
-        char[] expectedResult = {'a', 'b', 'e', 'f'};
-        assertArrayEquals(expectedResult, util.getQuadrantValues(0, 0));
+        Assert.assertNull(util.getQuadrantValues(-1, 0));
+        Assert.assertNull(util.getQuadrantValues(0, 4));
+        
+        char[] expectedResult1 = {'a', 'b', 'e', 'f'};
+        assertArrayEquals(expectedResult1, util.getQuadrantValues(0, 0));
+        
+        char[] expectedResult2 = {'k', 'l', 'o', 'p'};
+        assertArrayEquals(expectedResult2, util.getQuadrantValues(3, 3));
     }
 }
